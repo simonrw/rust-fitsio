6bc8b607ab6a9ca2d9f3767ec5bae9cd2946a0cb
Simon Walker <s.r.walker101@googlemail.com>
2016-06-01 20:52:01 +0100

    Add test for header card reading

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,6 +4,7 @@ extern crate tempdir;
 
 use std::ffi;
 use std::ptr;
+use libc::c_char;
 use fitsio::raw::*;
 
 #[test]
@@ -84,14 +85,60 @@ fn changing_hdu_by_absolute_number() {
     assert_eq!(hdu_num, 2);
 }
 
-/*
 #[test]
-fn api_usage() {
-    use fitsio::FitsFile;
+fn reading_header_key_value() {
+    let mut fptr = ptr::null_mut();
+    let mut status = -1;
+    let c_filename = ffi::CString::new("testdata/full_example.fits").unwrap();
 
-    let mut f = FitsFile::open("testdata/full_example.fits");
-    let mut primary_hdu = f.primary_hdu();
-    let header = primary_hdu.header();
-    let exposure_time: f32 = header["exposure"];
+    let mut long_value = 0;
+    let mut float_value = 0.0;
+    let mut double_value = 0.0;
+    let keyname = ffi::CString::new("INTTEST").unwrap();
+    let double_keyname = ffi::CString::new("DBLTEST").unwrap();
+    let mut comment: Vec<c_char> = vec![0; 73];
+    unsafe {
+        ffopen(&mut fptr as *mut *mut fitsfile,
+               c_filename.as_ptr(),
+               0,
+               &mut status);
+        ffgkyj(fptr,
+               keyname.as_ptr(),
+               &mut long_value,
+               ptr::null_mut(),
+               &mut status);
+        ffgkye(fptr,
+               keyname.as_ptr(),
+               &mut float_value,
+               ptr::null_mut(),
+               &mut status);
+
+        // Double version is different
+        ffgkyd(fptr,
+               double_keyname.as_ptr(),
+               &mut double_value,
+               comment.as_mut_ptr(),
+               &mut status);
+        ffclos(fptr, &mut status);
+    }
+
+    assert_eq!(long_value, 42);
+    assert_eq!(float_value, 42.0);
+    assert_eq!(double_value, 3. / 32.);
+
+    // TODO Hacky way of getting a string out. This should be simplified.
+    let comment: Vec<u8> = comment.iter().map(|&x| x as u8).filter(|&x| x != 0).collect();
+    let comment = String::from_utf8(comment).unwrap();
+    assert_eq!(comment, "Double value");
 }
-*/
+
+// #[test]
+// fn api_usage() {
+// use fitsio::FitsFile;
+//
+// let mut f = FitsFile::open("testdata/full_example.fits");
+// let mut primary_hdu = f.primary_hdu();
+// let header = primary_hdu.header();
+// let exposure_time: f32 = header["exposure"];
+// }
+//
