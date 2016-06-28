4831f51b334799d0f96e7e5eca76b995ab9b65bc
Simon Walker <s.r.walker101@googlemail.com>
2016-06-28 16:58:17 +0100

    Add open implementation

--- fitsio/src/lib.rs
+++ fitsio/src/lib.rs
@@ -23,6 +23,35 @@ pub struct FitsFile {
 }
 
 impl FitsFile {
+    fn open(filename: &str) -> Result<Self> {
+        let mut fptr = ptr::null_mut();
+        let mut status = 0;
+        let c_filename = ffi::CString::new(filename).unwrap();
+
+        unsafe {
+            sys::ffopen(&mut fptr as *mut *mut sys::fitsfile,
+                   c_filename.as_ptr(),
+                   sys::FileOpenMode::READONLY as libc::c_int,
+                   &mut status);
+        }
+
+        match status {
+            0 => {
+                Ok(FitsFile {
+                    fptr: fptr,
+                    filename: filename.to_string(),
+                })
+            }
+            status => {
+                Err(FitsError {
+                    status: status,
+                    message: stringutils::status_to_string(status).unwrap(),
+                })
+            }
+        }
+
+    }
+
     fn create(path: &str) -> Result<Self> {
         let mut fptr = ptr::null_mut();
         let mut status = 0;
@@ -30,8 +59,8 @@ impl FitsFile {
 
         unsafe {
             sys::ffinit(&mut fptr as *mut *mut sys::fitsfile,
-                               c_filename.as_ptr(),
-                               &mut status);
+                        c_filename.as_ptr(),
+                        &mut status);
         }
 
         match status {
@@ -64,6 +93,14 @@ impl Drop for FitsFile {
 mod test {
     extern crate tempdir;
     use super::FitsFile;
+
+    #[test]
+    fn opening_an_existing_file() {
+        match FitsFile::open("../testdata/full_example.fits") {
+            Ok(_) => {}
+            Err(e) => panic!("{:?}", e),
+        }
+    }
 
     #[test]
     fn creating_a_new_file() {
