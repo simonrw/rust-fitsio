7b139cc18bcea914c21ee1b8bcdbb37fd7031d13
Simon Walker <s.r.walker101@googlemail.com>
2017-03-09 17:19:28 +0000

    Remove todo

--- fitsio/src/columndescription.rs
+++ fitsio/src/columndescription.rs
@@ -5,7 +5,6 @@ use std::str::FromStr;
 pub struct ColumnDescription {
     pub name: String,
 
-    // TODO: make this use one of the enums
     /// Type of the data, see the cfitsio documentation
     pub data_type: ColumnDataDescription,
 }
