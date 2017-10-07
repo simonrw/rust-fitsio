c6442b637166aad80b8f6e613c1ead9939ac21df
Simon Walker <s.r.walker101@googlemail.com>
2017-10-07 10:51:01 +0100

    Stop testing with multiple threads on osx in test scripts

--- test_all.sh
+++ test_all.sh
@@ -3,6 +3,16 @@ set -e
 
 export CARGO_TARGET_DIR=target
 
+# Hack for OSX: we cannot run the tests in parallel
+case $OSTYPE in
+    darwin*)
+        __TESTEXTRA="-- --test-threads 1"
+        ;;
+    default)
+        __TESTEXTRA=""
+        ;;
+esac
+
 for toml in $(find . -maxdepth 2 -name "Cargo.toml"); do
-    echo $toml | grep -q bindgen || cargo test --manifest-path $toml
+    echo $toml | grep -q bindgen || cargo test --manifest-path $toml ${__TESTEXTRA}
 done
