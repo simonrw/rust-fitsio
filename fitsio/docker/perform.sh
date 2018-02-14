5b2722357fabdf9fb04b2b2c8842189d59951aa9
Simon Walker <s.r.walker101@googlemail.com>
2018-02-14 18:36:25 +0000

    Perform test properly

--- fitsio/docker/perform.sh
+++ fitsio/docker/perform.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 analyse_log() {
     grep -q cfitsio /tmp/logfile
 }
 
-main() {
-    cargo test --verbose -- --test-threads 1 2>&1 | tee /tmp/logfile
-    analyse_log
+reset() {
+    cargo clean
+}
 
-    cargo test --verbose --features bindgen --no-default-features -- --test-threads 1 2>&1 | tee /tmp/logfile
+test_default() {
+    reset
+    cargo test -j 1 --verbose -- --test-threads 1 2>&1 | tee /tmp/logfile
     analyse_log
+}
+
+test_bindgen() {
+    reset
+    cargo test -j 1 --verbose --features bindgen --no-default-features -- --test-threads 1 2>&1 | tee /tmp/logfile
+    analyse_log
+}
+
+main() {
+    test_default
+    test_bindgen
 }
 
 main
