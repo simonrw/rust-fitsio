61989521c8955b7777cb4e8b4dac0dfbd88310b9
Simon Walker <s.r.walker101@googlemail.com>
2018-02-23 11:23:36 +0000

    Clean up top level directory

--- build_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-export CARGO_TARGET_DIR=target
-
-for toml in $(find . -maxdepth 2 -name "Cargo.toml"); do
-    cargo doc --no-deps --manifest-path $toml
-done
--- upload_docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-export CARGO_TARGET_DIR=target
-
-ghp-import -n -p target/doc
