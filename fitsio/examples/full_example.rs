4a00c7632b011862588ea1a300c06a76a579be3d
Simon Walker <s.r.walker101@googlemail.com>
2018-03-03 20:23:51 +0000

    Convert all todo items to github issues

--- fitsio/examples/full_example.rs
+++ fitsio/examples/full_example.rs
@@ -3,24 +3,22 @@ extern crate tempdir;
 
 /* This example docuents the following things:
  *
- * TODO: make sure this list is up to date with the example below
- *
  * creating a new file
  * writing header keys
  * writing an image into the primary hdu
  * creating a table
+ * adding data to a table
  * closing the file
- * re-opening it and reading the data out
+ * re-opening it
+ * reading an image region
+ * reading some rows from a table column
  */
 
 use std::error::Error;
 use tempdir::TempDir;
 use fitsio::FitsFile;
-// TODO: move Image related structs/types into a more sensible place
 use fitsio::fitsfile::ImageDescription;
-// TODO: remove the import of imagetype from main package
 use fitsio::types::ImageType;
-// TODO: tidy up column description import
 use fitsio::columndescription::{ColumnDataType, ColumnDescription};
 
 fn run() -> Result<(), Box<Error>> {
@@ -30,11 +28,9 @@ fn run() -> Result<(), Box<Error>> {
 
     // creating a new file with 512 rows and 1024 columns
     let primary_hdu_description = ImageDescription {
-        // TODO: use a rust enum not C enum
         data_type: ImageType::DOUBLE_IMG,
         dimensions: &[512, 1024],
     };
-    // TODO: handle this error properly and/or handle this file path properly
 
     {
         let mut fitsfile = FitsFile::create(file_path.to_str().unwrap())
@@ -48,10 +44,9 @@ fn run() -> Result<(), Box<Error>> {
          */
 
         /* First we get the primary HDU */
-        // TODO: add primary hdu method => let hdu = fitsfile.primary_hdu()?;
         let hdu = fitsfile.hdu(0)?;
+
         /* Now we add the header keys */
-        // TODO: implement WritesKey for &str and String (AsRef<str>?)
         hdu.write_key(
             &mut fitsfile,
             "PROJECT",
@@ -62,7 +57,6 @@ fn run() -> Result<(), Box<Error>> {
         hdu.write_key(&mut fitsfile, "EXPTIME", 15.2f32)?;
 
         /* And finally the image id */
-        // TODO: implement write_key for u64?
         hdu.write_key(&mut fitsfile, "IMAGE_ID", 20180101010005i64)?;
 
         /* Now we write some dummy data to the primary HDU. We write the full image in this case.
@@ -72,11 +66,14 @@ fn run() -> Result<(), Box<Error>> {
             .map(|val| (val as f32) * 12.5 + 102.5)
             .collect();
 
-        // TODO: remove documentation from trait methods altogether
-        // TODO: document `write_image` in main lib documentation
         hdu.write_image(&mut fitsfile, &dummy_data)?;
 
-        // TODO: Adding a new image HDU?
+        /* We can create a new image with the following */
+        let image_description = ImageDescription {
+            data_type: ImageType::LONG_IMG,
+            dimensions: &[256, 256],
+        };
+        fitsfile.create_image("IMG".to_string(), &image_description)?;
 
         /* Now we add a new table HDU called "DATA"
          *
@@ -97,11 +94,9 @@ fn run() -> Result<(), Box<Error>> {
             .with_type(ColumnDataType::Float)
             .create()?;
         let columns = &[col1, col2, col3];
-        // TODO: create table should take &str or String (AsRef<str>?)
         let table_hdu = fitsfile.create_table("DATA".to_string(), columns)?;
 
         /* Add some data to the columns */
-        // TODO: add row methods
         let n_rows = 10;
         let obj_id_data: Vec<i32> = (0..n_rows).collect();
         let name_data: Vec<String> = (0..n_rows).map(|idx| format!("N{}", idx)).collect();
@@ -110,8 +105,6 @@ fn run() -> Result<(), Box<Error>> {
         table_hdu.write_col(&mut fitsfile, "OBJ_ID", &obj_id_data)?;
         table_hdu.write_col(&mut fitsfile, "NAME", &name_data)?;
         table_hdu.write_col(&mut fitsfile, "MAG", &mag_data)?;
-
-        // TODO: pretty print table structure?
 
         /* `fitsfile` is dropped here at the end of the scope, which closes fhe file on disk */
     }
@@ -122,10 +115,6 @@ fn run() -> Result<(), Box<Error>> {
     /* Get the primary HDU and read a section of the image data */
 
     let phdu = fitsfile.hdu(0)?;
-    // TODO: let phdu = fitsfile.primary_hdu()?;
-
-    // TODO: if we drop `fitsfile`, can we still use a FitsHdu object? What is the error that
-    // occurs?!
 
     /* Let's say we have a region around a star that we want to extract. The star is at (25, 25,
      * 1-indexed) and we want to extract a 5x5 box around it. This means we want to read rows 19 to
