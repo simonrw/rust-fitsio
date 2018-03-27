1b7556c7097b47591620d26774f1a166ddf03a0b
Simon Walker <s.r.walker101@googlemail.com>
2018-03-27 18:35:10 +0100

    Implement array for most functions through dynamically sized arrays

--- fitsio/src/ndarray_compat.rs
+++ fitsio/src/ndarray_compat.rs
@@ -5,10 +5,11 @@ use super::{FitsFile, FitsHdu};
 use super::fitsfile::ReadImage;
 use super::errors::Result;
 use super::types::HduInfo;
-use ndarray::{Array, Array1, Array2};
+use ndarray::{Array, ArrayD};
 
-impl<T> ReadImage for Array2<T>
+impl<T> ReadImage for ArrayD<T>
 where
+    T: Clone,
     Vec<T>: ReadImage,
 {
     fn read_section(
@@ -25,17 +26,16 @@ where
         start_row: usize,
         num_rows: usize,
     ) -> Result<Self> {
-        if num_rows == 0 || num_rows == 1 {
-            unimplemented!("not implemented for ndarray::Array2")
-        }
         let data: Vec<T> = ReadImage::read_rows(fits_file, hdu, start_row, num_rows)?;
         let arr = Array::from_vec(data);
         let row_length = arr.len() / num_rows;
-        Ok(arr.into_shape((num_rows, row_length)).unwrap())
+        Ok(arr.into_shape(vec![num_rows, row_length]).unwrap())
     }
 
     fn read_row(fits_file: &mut FitsFile, hdu: &FitsHdu, row: usize) -> Result<Self> {
-        Self::read_rows(fits_file, hdu, row, 1)
+        let data: Vec<T> = ReadImage::read_row(fits_file, hdu, row)?;
+        let shape = vec![data.len()];
+        Ok(Array::from_shape_vec(shape, data).unwrap())
     }
 
     fn read_region(
@@ -43,69 +43,24 @@ where
         hdu: &FitsHdu,
         ranges: &[&Range<usize>],
     ) -> Result<Self> {
-        if ranges.len() != 2 {
-            unimplemented!("only 2d regions can be read into an `ndarray::Array`");
-        }
-
         let data: Vec<T> = ReadImage::read_region(fits_file, hdu, ranges)?;
-        let arr = Array::from_vec(data);
-        let shape = (
-            ranges[0].end - ranges[0].start,
-            ranges[1].end - ranges[1].start,
-        );
-        println!("{:?}", ranges);
-        println!("{:?} {}", shape, arr.len());
-        Ok(arr.into_shape(shape).unwrap())
+        let shape: Vec<usize> = (0..ranges.len())
+            .map(|i| ranges[i].end - ranges[i].start)
+            .collect();
+        let arr = Array::from_shape_vec(shape, data).unwrap();
+        Ok(arr)
     }
 
     fn read_image(fits_file: &mut FitsFile, hdu: &FitsHdu) -> Result<Self> {
-        let data: Vec<T> = ReadImage::read_image(fits_file, hdu)?;
-        let arr = Array::from_vec(data);
         match hdu.info {
             HduInfo::ImageInfo { ref shape, .. } => {
-                Ok(arr.into_shape((shape[0], shape[1])).unwrap())
+                let data: Vec<T> = ReadImage::read_image(fits_file, hdu)?;
+                let shape: Vec<usize> = (0..2).map(|i| shape[i]).collect();
+                let arr = Array::from_shape_vec(shape, data).unwrap();
+                Ok(arr)
             }
             _ => unreachable!(),
         }
-    }
-}
-
-impl<T> ReadImage for Array1<T>
-where
-    Vec<T>: ReadImage,
-{
-    fn read_section(
-        _fits_file: &mut FitsFile,
-        _hdu: &FitsHdu,
-        _range: Range<usize>,
-    ) -> Result<Self> {
-        unimplemented!()
-    }
-
-    fn read_rows(
-        _fits_file: &mut FitsFile,
-        _hdu: &FitsHdu,
-        _start_row: usize,
-        _num_rows: usize,
-    ) -> Result<Self> {
-        unimplemented!()
-    }
-
-    fn read_row(fits_file: &mut FitsFile, hdu: &FitsHdu, row: usize) -> Result<Self> {
-        let data: Vec<T> = ReadImage::read_row(fits_file, hdu, row)?;
-        Ok(Array::from_vec(data))
-    }
-
-    fn read_region(
-        _fits_file: &mut FitsFile,
-        _hdu: &FitsHdu,
-        _ranges: &[&Range<usize>],
-    ) -> Result<Self> {
-        unimplemented!()
-    }
-
-    fn read_image(_fits_file: &mut FitsFile, _hdu: &FitsHdu) -> Result<Self> {
-        unimplemented!()
     }
 }
 
@@ -118,8 +73,10 @@ mod tests {
         let mut f = FitsFile::open("../testdata/full_example.fits").unwrap();
         let hdu = f.primary_hdu().unwrap();
 
-        let data: Array2<u32> = hdu.read_image(&mut f).unwrap();
-        assert_eq!(data.dim(), (100, 100));
+        let data: ArrayD<u32> = hdu.read_image(&mut f).unwrap();
+        let dim = data.dim();
+        assert_eq!(dim[0], 100);
+        assert_eq!(dim[1], 100);
         assert_eq!(data[[20, 5]], 152);
     }
 
@@ -128,8 +85,10 @@ mod tests {
         let mut f = FitsFile::open("../testdata/full_example.fits").unwrap();
         let hdu = f.primary_hdu().unwrap();
 
-        let data: Array2<u32> = hdu.read_rows(&mut f, 0, 2).unwrap();
-        assert_eq!(data.dim(), (2, 100));
+        let data: ArrayD<u32> = hdu.read_rows(&mut f, 0, 2).unwrap();
+        let dim = data.dim();
+        assert_eq!(dim[0], 2);
+        assert_eq!(dim[1], 100);
         assert_eq!(data[[1, 52]], 184);
     }
 
@@ -138,7 +97,7 @@ mod tests {
         let mut f = FitsFile::open("../testdata/full_example.fits").unwrap();
         let hdu = f.primary_hdu().unwrap();
 
-        let data: Array1<u32> = hdu.read_row(&mut f, 49).unwrap();
+        let data: ArrayD<u32> = hdu.read_row(&mut f, 49).unwrap();
         assert_eq!(data[20], 156);
     }
 
@@ -147,8 +106,10 @@ mod tests {
         let mut f = FitsFile::open("../testdata/full_example.fits").unwrap();
         let hdu = f.primary_hdu().unwrap();
 
-        let data: Array2<u32> = hdu.read_region(&mut f, &[&(70..80), &(20..50)]).unwrap();
-        assert_eq!(data.dim(), (10, 30));
+        let data: ArrayD<u32> = hdu.read_region(&mut f, &[&(70..80), &(20..50)]).unwrap();
+        let dim = data.dim();
+        assert_eq!(dim[0], 10);
+        assert_eq!(dim[1], 30);
         assert_eq!(data[[5, 10]], 160);
     }
 }
