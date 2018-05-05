d4b94aec1c2f8f5c779a89dc90cf911b327dab95
Simon Walker <s.r.walker101@googlemail.com>
2018-05-05 18:03:34 +0100

    Add setting and getting

--- fitsio/src/nullvec.rs
+++ fitsio/src/nullvec.rs
@@ -18,7 +18,11 @@ pub struct NullVec<T> {
 
 // TODO: check that data and nullvals are the same length
 
-impl<T> NullVec<T> {
+impl<T> NullVec<T>
+where
+    // Do we really want to specify Copy here?
+    T: Default + Clone + Copy,
+{
     /// Create a new null vector
     pub fn new() -> Self {
         NullVec {
@@ -43,6 +47,42 @@ impl<T> NullVec<T> {
         assert_eq!(self.data.len(), self.nullvals.len());
         self.data.capacity()
     }
+
+    pub fn push(&mut self, value: Option<T>) {
+        match value {
+            Some(v) => {
+                self.data.push(v);
+                self.nullvals.push(true);
+            }
+            None => {
+                self.data.push(Default::default());
+                self.nullvals.push(false);
+            }
+        }
+    }
+
+    pub fn set(&mut self, idx: usize, value: Option<T>) {
+        assert!(self.len() >= (idx + 1));
+        match value {
+            Some(v) => {
+                self.data[idx] = v;
+                self.nullvals.set(idx, true);
+            }
+            None => {
+                self.data[idx] = Default::default();
+                self.nullvals.set(idx, false);
+            }
+        }
+    }
+
+    pub fn get(&mut self, idx: usize) -> Option<T> {
+        assert!(self.len() >= (idx + 1));
+        if self.nullvals[idx] {
+            Some(self.data[idx])
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -60,5 +100,45 @@ mod tests {
         let n: NullVec<i32> = NullVec::with_capacity(10);
         assert_eq!(n.len(), 0);
         assert_eq!(n.capacity(), 10);
+    }
+
+    #[test]
+    fn test_appending() {
+        let mut n: NullVec<i32> = NullVec::new();
+        n.push(Some(10i32));
+        assert_eq!(n.len(), 1);
+
+        n.push(None);
+        assert_eq!(n.len(), 2);
+    }
+
+    #[test]
+    fn test_setting_valid_value() {
+        let mut n: NullVec<i32> = NullVec::new();
+        n.push(Some(10i32));
+        assert_eq!(n.len(), 1);
+
+        n.set(0, Some(10i32));
+        assert_eq!(n.len(), 1);
+    }
+
+    #[test]
+    fn test_setting_invalid_value() {
+        let mut n: NullVec<i32> = NullVec::new();
+        n.push(Some(10i32));
+        assert_eq!(n.len(), 1);
+
+        n.set(0, None);
+        assert_eq!(n.len(), 1);
+        assert_eq!(n.get(0), None);
+    }
+
+    #[test]
+    fn test_fetching_at_index() {
+        let mut n: NullVec<i32> = NullVec::new();
+        n.push(Some(10i32));
+        assert_eq!(n.len(), 1);
+
+        assert_eq!(n.get(0), Some(10i32));
     }
 }
