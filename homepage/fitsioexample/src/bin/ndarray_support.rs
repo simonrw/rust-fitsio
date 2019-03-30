e9311f49d9f2e8534196f354a83d5d7d5946c040
Simon Walker <s.r.walker101@googlemail.com>
2019-03-30 16:15:45 +0000

    Format example code

--- homepage/fitsioexample/src/bin/ndarray_support.rs
+++ homepage/fitsioexample/src/bin/ndarray_support.rs
@@ -1,19 +1,21 @@
 extern crate fitsio;
 extern crate ndarray;
 
-use std::error::Error;
 use fitsio::FitsFile;
 use ndarray::ArrayD;
+use std::error::Error;
 
 fn try_main() -> Result<(), Box<Error>> {
-let filename = "../testdata/full_example.fits";
-let mut fptr = FitsFile::open(filename)?;
-let phdu = fptr.primary_hdu()?;
-let image: ArrayD<f32> = phdu.read_image(&mut fptr)?;
-let new_image = (image * 4.0) / 5.2;
-assert_eq!(new_image.ndim(), 2);
+    let filename = "../testdata/full_example.fits";
+    let mut fptr = FitsFile::open(filename)?;
+    let phdu = fptr.primary_hdu()?;
+    let image: ArrayD<f32> = phdu.read_image(&mut fptr)?;
+    let new_image = (image * 4.0) / 5.2;
+    assert_eq!(new_image.ndim(), 2);
 
-Ok(())
+    Ok(())
 }
 
-fn main() { try_main().unwrap() }
+fn main() {
+    try_main().unwrap()
+}
