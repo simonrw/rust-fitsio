e13a60e3cb602198e63676b92b769bbc7a326e0b
Simon Walker <s.r.walker101@googlemail.com>
2020-04-01 01:12:11 +0100

    Adds ability to check errors and show them all

--- bin/run_sanitizers.sh
+++ bin/run_sanitizers.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+errCounter() {
+    (( errcount++ ))
+}
+
 main() {
 
     # Assume we are in a subdirectory of `fitsio` and move upwards
@@ -15,15 +19,30 @@ main() {
         cd ..
     done
 
+
     # Check the number of arguments. If the user supplies an argument, assume
     # they want to run a single sanitiser.
     if [[ $# -eq 0 ]]; then
+
+        # Should any errors happen, then enter the `errCounter` function which
+        # increments the number of errors that happen.
+        trap errCounter ERR
+
+        set +e
         for sanitizer in address memory; do
             RUSTFLAGS="-Z sanitizer=$sanitizer" cargo +nightly run \
                 --manifest-path fitsio/Cargo.toml \
                 --example full_example \
                 --target x86_64-unknown-linux-gnu || true
         done
+
+        set -e
+
+        if [[ $errcount -ne 0 ]]; then
+            echo "Program had $errcount failures" >&2
+            exit 1
+        fi
+
     elif [[ $# -eq 1 ]]; then
         sanitizer=$1
         RUSTFLAGS="-Z sanitizer=$sanitizer" cargo +nightly run \
