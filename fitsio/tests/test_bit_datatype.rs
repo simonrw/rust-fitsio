7b90652b6758ea4a7690c49a9963b453730c85c6
Simon Walker <s.r.walker101@googlemail.com>
2020-05-22 23:00:08 +0100

    Adds test for writing data

--- fitsio/tests/test_bit_datatype.rs
+++ fitsio/tests/test_bit_datatype.rs
@@ -1,5 +1,7 @@
+use fitsio::tables::{ColumnDataType, ColumnDescription};
 use fitsio::FitsFile;
 use std::path::Path;
+use tempfile::Builder;
 
 #[test]
 fn test_reading_bit_data_type() {
@@ -9,4 +11,32 @@ fn test_reading_bit_data_type() {
     let table_hdu = f.hdu(1).unwrap();
     let flags: Vec<u32> = table_hdu.read_col(&mut f, "FLAGS").unwrap();
     assert_eq!(flags.len(), 1_849_344);
+}
+
+#[test]
+fn test_writing_bit_data_type() {
+    /* Create a temporary directory to work from */
+    let tmp_dir = Builder::new().prefix("fitsio-").tempdir().unwrap();
+    let file_path = tmp_dir.path().join("example.fits");
+
+    {
+        let mut fitsfile = FitsFile::create(&file_path).open().unwrap();
+
+        let data: Vec<u32> = (0..64).collect();
+        let col = ColumnDescription::new("BITMASK")
+            .with_type(ColumnDataType::Bit)
+            .create()
+            .unwrap();
+        let columns = &[col];
+        let table_hdu = fitsfile.create_table("DATA", columns).unwrap();
+        table_hdu
+            .write_col(&mut fitsfile, "BITMASK", &data)
+            .unwrap();
+    }
+
+    let mut f = FitsFile::open(file_path).unwrap();
+
+    let table_hdu = f.hdu("DATA").unwrap();
+    let flags: Vec<u32> = dbg!(table_hdu.read_col(&mut f, "BITMASK").unwrap());
+    assert_eq!(flags.len(), 64);
 }
