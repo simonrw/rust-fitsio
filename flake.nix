eb84eb0ae9c352d7afd7450bf093da22c35e4a61
Simon Walker <s.r.walker101@googlemail.com>
2022-10-15 10:59:19 +0100

    fix versions with flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}
