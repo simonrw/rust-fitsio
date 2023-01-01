1aebb8ee534a2c1d6c7ccb0b42c8c070f3e54fe8
Simon Walker <s.r.walker101@googlemail.com>
2023-01-01 22:36:15 +0000

    Fix bindgen compilation on NixOS

--- flake.nix
+++ flake.nix
@@ -16,6 +16,22 @@
             # for bin/test
             pkgs.python3
           ];
+          shellHook = ''
+            # From: https://github.com/NixOS/nixpkgs/blob/1fab95f5190d087e66a3502481e34e15d62090aa/pkgs/applications/networking/browsers/firefox/common.nix#L247-L253
+            # Set C flags for Rust's bindgen program. Unlike ordinary C
+            # compilation, bindgen does not invoke $CC directly. Instead it
+            # uses LLVM's libclang. To make sure all necessary flags are
+            # included we need to look in a few places.
+            #
+            # source: https://hoverbear.org/blog/rust-bindgen-in-nix/
+            export BINDGEN_EXTRA_CLANG_ARGS="$(< ${pkgs.stdenv.cc}/nix-support/libc-crt1-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/libc-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/cc-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/libcxx-cxxflags) \
+              ${pkgs.lib.optionalString pkgs.stdenv.cc.isClang "-idirafter ${pkgs.stdenv.cc.cc}/lib/clang/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/include"} \
+              ${pkgs.lib.optionalString pkgs.stdenv.cc.isGNU "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${pkgs.lib.getVersion pkgs.stdenv.cc.cc} -isystem ${pkgs.stdenv.cc.cc}/include/c++/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/${pkgs.stdenv.hostPlatform.config} -idirafter ${pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/include"} \
+              "
+          '';
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
         };
       }
