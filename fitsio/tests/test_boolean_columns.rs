b2ba5c2bbbf66827f789f685edb2b46ad20e5b0e
Simon Walker <s.r.walker101@googlemail.com>
2024-07-26 22:48:26 +0100

    Remove pretty_print from tests

--- fitsio/tests/test_boolean_columns.rs
+++ fitsio/tests/test_boolean_columns.rs
@@ -24,7 +24,6 @@ static EXPECTED: &[bool] = &[
 #[test]
 fn reading_binary_columns() -> Result<(), Box<dyn std::error::Error>> {
     let mut fitsfile = FitsFile::open("../testdata/boolean_columns.fits")?;
-    fitsfile.pretty_print().expect("printing fits file");
     let ant_hdu = fitsfile.hdu(1)?;
     let col = ant_hdu.read_col::<bool>(&mut fitsfile, "Whitening_Filter")?;
     assert_eq!(col, EXPECTED);
