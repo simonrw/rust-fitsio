e40ed46b18bf0951127da9bee440b563c1491a2a
Simon Walker <s.r.walker101@googlemail.com>
2024-12-30 23:24:40 +0000

    Use custom target dir for project

--- flake.nix
+++ flake.nix
@@ -32,6 +32,12 @@
           RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
 
           shellHook = ''
+            export RUST_BUILD_BASE="$HOME/.cache/rust-builds"
+            WORKSPACE_ROOT=$(cargo metadata --no-deps --offline 2>/dev/null | jq -r ".workspace_root")
+            PACKAGE_BASENAME=$(basename $WORKSPACE_ROOT)
+            # Run cargo with target set to $RUST_BUILD_BASE/$PACKAGE_BASENAME
+            export CARGO_TARGET_DIR="$RUST_BUILD_BASE/$PACKAGE_BASENAME"
+
             # From: https://github.com/NixOS/nixpkgs/blob/1fab95f5190d087e66a3502481e34e15d62090aa/pkgs/applications/networking/browsers/firefox/common.nix#L247-L253
             # Set C flags for Rust's bindgen program. Unlike ordinary C
             # compilation, bindgen does not invoke $CC directly. Instead it
