7ccba3f94cd22e3a584c55eea23fd57c9e0d9c11
Zachary Catlin <zec0@psu.edu>
2026-03-26 16:19:33 -0400

    Revamped Rust type-to-DataType mapping
    This change fixes a bug found in use. Our team tried to write
    header entries with 64-bit values on a 32-bit ARM system, but found that
    the high-order 32 bits were stripped off in the resulting FITS file.
    
    Investigation found that `fitsio`, when using write_key with a 64-bit
    {signed, unsigned} integer, was using DataType::{TLONG, TULONG}.
    This instructs CFITSIO to treat the passed-in value as a C
    {long, unsigned long}.
    
    On some systems, C long is 64 bits long; this is *not* the case
    on 32-bit ARM however, where long is 32 bits, causing only 32 bits
    of the value to be written.
    
    This commit does not try to match Rust types to C types ABI-by-ABI, but
    instead associates each relevant integer and floating-point type
    with a value of DataType that is determined at compile-time to be an
    equivalent C type.

--- fitsio/src/types.rs
+++ fitsio/src/types.rs
@@ -24,6 +24,120 @@ pub enum DataType {
     TDBLCOMPLEX,
 }
 
+/// A trait to associate a type with the appropriate [`DataType`].
+///
+/// This helper trait is useful as the different values of [`DataType`],
+/// as used by CFITSIO, refer to **C** types, not **Rust** types.
+/// Some compile-time calculations are used to ensure the right C type
+/// is associated with the Rust type.
+pub(crate) trait HasFitsDataType {
+    /// The [`DataType`] associated with `Self`.
+    const FITS_DATA_TYPE: DataType;
+}
+
+/// Convenience function; returns the size and alignment of `T`.
+const fn size_align<T: Sized>() -> (usize, usize) {
+    (core::mem::size_of::<T>(), core::mem::align_of::<T>())
+}
+
+/// Macro to compute the signedness, size, and alignment of a type.
+///
+/// `$name` will be the constant `(is $t unsigned, (size of $t, alignment of $t))`.
+macro_rules! ctype_sign_sz_align {
+    ($($t:ty, $name:ident);* $(;)?) => {
+        $(
+            // we use $t::MIN == 0 here and in has_fits_data_type_int! to distinguish between signed and unsigned types
+            const $name: (bool, (usize, usize)) = (<$t>::MIN == 0, size_align::<$t>());
+        )*
+    };
+}
+
+ctype_sign_sz_align!(
+    core::ffi::c_schar,  SCHAR;
+    core::ffi::c_uchar,  UCHAR;
+    core::ffi::c_short,  SHORT;
+    core::ffi::c_ushort, USHORT;
+
+    core::ffi::c_int,    INT;
+    core::ffi::c_uint,   UINT;
+    core::ffi::c_long,   LONG;
+    core::ffi::c_ulong,  ULONG;
+
+    core::ffi::c_longlong,  LONGLONG;
+    core::ffi::c_ulonglong, ULONGLONG;
+);
+
+/// Generates an implementation of [`HasFitsDataType`] for integer type `$t`,
+/// making sure we match `$t` with an equivalent C type.
+macro_rules! has_fits_data_type_int {
+    ($t:ty) => {
+        impl HasFitsDataType for $t {
+            const FITS_DATA_TYPE: DataType = {
+                // we use $t::MIN == 0 here and in ctype_sign_sz_align! to distinguish between signed and unsigned types
+
+                #[allow(unreachable_patterns)]
+                match (<$t>::MIN == 0, size_align::<$t>()) {
+                    UINT => DataType::TUINT,
+                    USHORT => DataType::TUSHORT,
+                    UCHAR => DataType::TBYTE,
+                    ULONG => DataType::TULONG,
+                    ULONGLONG => DataType::TULONGLONG,
+
+                    INT => DataType::TINT,
+                    SHORT => DataType::TSHORT,
+                    SCHAR => DataType::TSBYTE,
+                    LONG => DataType::TLONG,
+                    LONGLONG => DataType::TLONGLONG,
+
+                    _ => panic!(concat!(
+                        "Type ",
+                        stringify!($t),
+                        " does not have a corresponding DataType"
+                    )),
+                }
+            };
+        }
+    };
+}
+
+has_fits_data_type_int!(u8);
+has_fits_data_type_int!(u16);
+has_fits_data_type_int!(u32);
+has_fits_data_type_int!(u64);
+has_fits_data_type_int!(i8);
+has_fits_data_type_int!(i16);
+has_fits_data_type_int!(i32);
+has_fits_data_type_int!(i64);
+
+/// Generates an implementation of [`HasFitsDataType`] for floating-point type `$t`,
+/// making sure we match `$t` with an equivalent C type.
+macro_rules! has_fits_data_type_floating {
+    ($t:ty) => {
+        impl HasFitsDataType for $t {
+            const FITS_DATA_TYPE: DataType = {
+                use core::ffi::{c_double, c_float};
+
+                const FLOAT: (usize, usize) = size_align::<c_float>();
+                const DOUBLE: (usize, usize) = size_align::<c_double>();
+
+                #[allow(unreachable_patterns)]
+                match size_align::<$t>() {
+                    DOUBLE => DataType::TDOUBLE,
+                    FLOAT => DataType::TFLOAT,
+                    _ => panic!(concat!(
+                        "Type ",
+                        stringify!($t),
+                        " does not have a corresponding DataType"
+                    )),
+                }
+            };
+        }
+    };
+}
+
+has_fits_data_type_floating!(f32);
+has_fits_data_type_floating!(f64);
+
 #[cfg(test)]
 mod test {
     use crate::fitsfile::{CaseSensitivity, FileOpenMode};
